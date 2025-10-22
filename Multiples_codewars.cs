dd5840b2cc3138417cd3f166b127338f99f137b3
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2025-10-22 15:24:49 +0300

    Add files via upload

---
+++ Multiples_codewars.cs
@@ -0,0 +1,33 @@
+﻿/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Additionally, if the number is negative, return 0.
+
+Note: If a number is a multiple of both 3 and 5, only count it once.*/
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Your number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int summa = 0;
+        if (number < 0)
+        {
+            Console.WriteLine(0);
+        }
+        else
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0 || i % 3 == 0 || i % 5 == 0)
+                {
+                    summa += i;
+                }
+            }
+            Console.WriteLine(summa);
+        }
+    }
+}
