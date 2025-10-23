62cfad06d0d73de984d77d25d6dcc889b14e619e
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2025-10-23 19:36:51 +0300

    Add files via upload

---
+++ codewars_StrokeReverse.cs
@@ -0,0 +1,14 @@
+﻿// Complete the solution so that it reverses the string passed into it.
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Your string: ");
+        string str = Console.ReadLine();
+        char[] arr = str.ToCharArray();
+        Array.Reverse(arr);
+        Console.WriteLine(arr);
+    }
+}
