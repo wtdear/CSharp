ddcc2d3575aaa74464987cc8d5b649b1f68702f2
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2025-10-24 19:02:22 +0300

    Add files via upload

---
+++ codewars_Negative.cs
@@ -0,0 +1,14 @@
+﻿// In this simple assignment you are given a number and have to make it negative.
+// But maybe the number is already negative?
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Your number: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(num >= 0 ? num * -1 : num);
+    }
+}
