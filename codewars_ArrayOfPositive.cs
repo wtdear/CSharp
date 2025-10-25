489152a642962ab56c25b1b8899b040f466f0861
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2025-10-25 18:46:11 +0300

    Add files via upload

---
+++ codewars_ArrayOfPositive.cs
@@ -0,0 +1,21 @@
+﻿//You get an array of numbers, return the sum of all of the positives ones. [1, -4, 7, 12]
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> list = [1, -4, 7, 12];
+        int sum = 0;
+
+        foreach (int number in list)
+        {
+            if (number > 0)
+            {
+                sum += number;
+            }
+        }
+        Console.WriteLine(sum);
+    }
+}
