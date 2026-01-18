64476575fee5996d28623291a0eec8ed53a2c764
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2026-01-18 12:08:16 +0300

    Add files via upload

---
+++ findNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int number = 100; number <= 999; number++)
+        {
+            int lastDigit = number % 10;
+            int subs = number - lastDigit;
+            int divided = subs / 10;
+            int result = lastDigit * 100 + divided;
+            
+            if (result == 237)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
