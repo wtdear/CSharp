93a4f9adc808b191c22ba175ce84ea4acbb43a62
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2026-01-19 20:40:06 +0300

    Add files via upload

---
+++ taskLocationX/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение X: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        if (x > 0)
+        {
+            double y = Math.Sin(x);
+            Console.WriteLine($"Ваш Y: {y}"); 
+        }
+        else
+        {
+            double y = Math.Cos(x);
+            Console.WriteLine($"Ваш Y: {y}"); 
+        }
+    }
+}
