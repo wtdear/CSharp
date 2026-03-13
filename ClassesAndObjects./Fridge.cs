1adafed0181773ac205bd154627a6802fe159036
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2026-03-13 15:43:49 +0300

    Add files via upload

---
+++ ClassesAndObjects./Fridge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task1
+{
+	public class Fridge : OnOff
+	{
+		internal string modelName;
+		
+		public void On()
+		{
+			Console.Write($"Fridge '{modelName}' turned on");
+		}
+
+		public void Off()
+		{
+			Console.Write($"Fridge '{modelName}' turned off");
+		}
+	}
+}
+
---
+++ ClassesAndObjects./inteface.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public interface OnOff
+{
+    void On();
+    void Off();
+}
