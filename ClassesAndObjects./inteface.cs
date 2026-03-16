67bb16f917fb37367a31c3384e06b9066fd23929
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2026-03-16 13:17:45 +0300

    Update inteface.cs

--- ClassesAndObjects./inteface.cs
+++ ClassesAndObjects./inteface.cs
@@ -1,7 +1,12 @@
-﻿using System;
+using System;
 
 public interface OnOff
 {
     void On();
     void Off();
+}
+public interface IReservable
+{
+    void MakeReservation();
+    void DeleteReservation();
 }
