f8e0e98b916b5985d007637a8a6e6f91aa99e757
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2026-03-16 13:19:59 +0300

    Add files via upload

---
+++ AirTicket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class AirTicket : IReservable
+{
+    internal string Name;
+    internal bool Status;
+
+    public void MakeReservation()
+    {
+        Console.WriteLine($"Вы успешно забронировали рейс '{Name}' | Статус - {Status}");
+    }
+
+    public void DeleteReservation()
+    {
+        Console.WriteLine($"Вы успешно удалили бронь рейса '{Name}' | Статус - {Status}");
+    }
+}
