fd349debc88089c8c440febf504e85655030b637
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2026-03-24 20:05:29 +0300

    Update Program.cs

--- ClassesAndObjects./Program.cs
+++ ClassesAndObjects./Program.cs
@@ -112,4 +112,173 @@ Choice - ");
 
 					Hotel hotel = new Hotel();
 					hotel.Name = Name;
-					hotel.Status = S
+					hotel.Status = Status;
+
+					hotel.MakeReservation();
+                }
+                else
+                {
+					Console.Clear();
+
+                    Console.WriteLine("Reservation already taken!");
+                    hotelReservation();
+                }
+                return;
+			default:
+				Console.Clear();
+				hotelReservation();
+                break;
+		}
+	} // HotelReservation
+
+    static void airReservation()
+    {
+        Console.Clear();
+
+        Console.Write(@"What you want?:
+
+1) Delete Reservation
+2) Get Reservation");
+
+        string airChoose = Console.ReadLine();
+        bool Status = false;
+        string Name;
+
+        switch (airChoose)
+        {
+            case "1":
+                if (Status)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("You already have reservation!\n");
+                    hotelReservation();
+                }
+                else
+                {
+                    Console.Write("Enter air ticket name: ");
+                    Name = Console.ReadLine();
+
+                    Console.WriteLine("Reservation was deleted");
+                    Status = false;
+
+                    AirTicket air = new AirTicket();
+                    air.Name = Name;
+                    air.Status = Status;
+
+                    air.DeleteReservation();
+                }
+                return;
+            case "2":
+                if (Status == false)
+                {
+                    Console.Write("Enter air ticket name: ");
+                    Name = Console.ReadLine();
+                    Status = true;
+
+					AirTicket air = new AirTicket();
+					air.Name = Name;
+					air.Status = Status;
+
+					air.MakeReservation();
+                }
+                else
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("Reservation already taken!");
+                    hotelReservation();
+                }
+                return;
+            default:
+                Console.Clear();
+                hotelReservation();
+                break;
+        }
+    } // AirReservation
+	
+    static void Devices()
+	{
+		Console.Clear();
+
+		Console.Write("Enter device name: ");
+		string deviceName = Console.ReadLine();
+
+		Console.Write(@"
+Enter device type:
+1) TV
+2) Fridge
+Type: ");
+		string deviceChoice = Console.ReadLine();
+
+		switch (deviceChoice)
+		{
+			case "1":
+				FunctionsTV(deviceName);
+				break;
+			case "2":
+				FunctionsFridge(deviceName);
+				break;
+			default:
+				Console.Clear();
+				Devices();
+				break;
+		}
+    } // Devices
+
+	static void FunctionsTV(string deviceName)
+	{
+        TV tv = new TV();
+        tv.modelName = deviceName;
+
+		Console.Write(@"
+Do you want On or Off TV?:
+1) On
+2) Off
+Choice - ");
+
+        string TVFuncChoice = Console.ReadLine();
+
+        switch (TVFuncChoice)
+        {
+            case "1":
+                tv.On();
+                break;
+            case "2":
+                tv.Off();
+                break;
+            default:
+                Console.Clear();
+                Devices();
+                break;
+        }
+    } // FunctionsTV
+
+    static void FunctionsFridge(string deviceName)
+	{
+        Fridge fridge = new Fridge();
+        fridge.modelName = deviceName;
+
+        Console.Write(@"
+Do you want On or Off Fridge?:
+1) On
+2) Off
+Choice - ");
+
+		string fridgeFuncChoice = Console.ReadLine();
+
+		switch (fridgeFuncChoice)
+		{
+			case "1":
+				fridge.On();
+				break;
+			case "2":
+				fridge.Off();
+				break;
+			default:
+				Console.Clear();
+				Devices();
+				break;
+		}
+	} // FunctionsFridge
+}
